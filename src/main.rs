c3134092a70c4899df78a488d0e00f6b03d34a82
rene paulokat <rene@so36.net>
2017-02-02 23:59:06 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
+mod codes;
+
 #[macro_use]
 extern crate nickel;
 
-use nickel::{Nickel, HttpRouter, StaticFilesHandler};
+use nickel::{Nickel, StaticFilesHandler};
+use nickel::status::StatusCode;
 use std::collections::HashMap;
 
 
 fn main() {
     let mut server = Nickel::new();
 
-
     server.utilize(router! {
-
-        get "/status/:status_code" => |req, res| {
+        get "/status/:status_code" => |req, mut res| {
             let mut data = HashMap::new();
-            data.insert("content", "here some content");
-            data.insert("status_code", req.param("status_code").unwrap());
+            let arg = req.param("status_code").unwrap();
+            let i_arg = arg.parse::<u16>().unwrap();
+            data.insert("status_code", arg);
+            res.set(extract_status_code(i_arg));
             return res.render("assets/templates/layout.tpl", &data);
         }
     });
     server.utilize(StaticFilesHandler::new("assets/templates"));
     server.listen("127.0.0.1:6767");
+}
+
+
+fn extract_status_code(sc: u16) -> StatusCode  {
+    return StatusCode::from_u16(sc)
 }
