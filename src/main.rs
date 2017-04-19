0a4cc5bab553764f84f1b854b17b2e67add5c5e3
rene paulokat <rene@so36.net>
2017-04-19 14:34:00 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate nickel;
 use nickel::{Nickel, StaticFilesHandler};
 use nickel::status::StatusCode;
 use std::collections::HashMap;
-
+use std::env;
 
 fn main() {
     let mut server = Nickel::new();
@@ -22,10 +22,20 @@ fn main() {
         }
     });
     server.utilize(StaticFilesHandler::new("assets/templates"));
-    server.listen("0.0.0.0:8080");
+    server.listen(&*make_address());
 }
 
 
 fn extract_status_code(sc: u16) -> StatusCode  {
     return StatusCode::from_u16(sc)
+}
+
+fn get_server_port() -> u16 {
+    let port_str = env::var("PORT").unwrap_or(String::new());
+    port_str.parse().unwrap_or(8080)
+}
+
+fn make_address() -> String {
+    let address = "0.0.0.0:".to_string();
+    address + &get_server_port().to_string()
 }
